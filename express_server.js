dd87ec0833fdecd506d9f6db3977b45871d56c3a
Duncan Haran <duncan.haran@gmail.com>
2019-07-08 21:37:08 +0000

    added a form submission for the user

--- express_server.js
+++ express_server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok");         
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
